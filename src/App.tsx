d462ffeaa73f79819909b30ebd39cac5aaa4a1ee
Juan Pablo Castro <158854560+Juancastrog10@users.noreply.github.com>
2026-04-27 18:36:24 -0500

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { Link, Route, Routes } from "react-router-dom";
+import { SpacesPage } from "./pages/SpacesPages";
+import { ReservationsPage } from "./pages/ReservationsPage";
+
+function App() {
+  return (
+    <div>
+      <nav>
+        <Link to="/">Espacios</Link>
+        <Link to="/reservations">Reservas</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<SpacesPage />} />
+        <Route path="/reservations" element={<ReservationsPage />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
